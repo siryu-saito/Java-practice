fafe6aa79714adc824d55e2bfb0a1f120cdeb237
siryu-saito <81666082+siryu-saito@users.noreply.github.com>
2021-12-27 17:05:58 +0900

    修正点
    修正点がありました

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
-		System.out.println("北村");
+		for (int i = 1; i < 102; i++) {
+			System.out.println("北村です");
+			if (i < 20) {
+				System.out.println("ウヒャヒャ、タバコおいちぃーーーー");
+			}　 else if (i == 30) {
+				System.out.println("タバコやめた方がいいってま？");
+			}  else if (i > 70) {
+				System.out.println("ウヒャヒャ、タバコおいちぃーーーー");
+			}
+		}
 		System.out.println("102歳です");
 		System.out.println("口腔癌です");
 		System.out.println("でも、煙草はやめねぇですぅ！");
