a9ea7383a5ac790dfdab62c8e8f87d734048871b
Demi <gorigoriuhouho0820@gmail.com>
2021-12-27 15:43:24 +0900

    warm-up

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 
 public class Main {
-
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("北村");
+		System.out.println("102歳です");
+		System.out.println("口腔癌です");
+		System.out.println("でも、煙草はやめねぇですぅ！");
+		System.out.println(51 + 51 + "歳です");
+		
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println("答えは" + 64);
+		
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
 }
